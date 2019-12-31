20752101c6be3e2e7a752d6b34fcc4864b52eb2a
Jason Walter <jaason@protonmail.ch>
2019-12-31 00:13:22 -0500

    fixed licensing stuff because why not

--- bright.sh
+++ bright.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # stupid wrapper. edit this to your preferences so it's comfy in your $PATH 
+# also this isn't licensed because that would be mega stupid
 
 sudo sh -c "./bright $1 $2"
